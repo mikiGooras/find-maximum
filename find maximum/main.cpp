1acd6f07e3b775cc5f7eb8f12d3a7939f6aefcbc
Michał Górny <michal.x.gorny@gmail.com>
2022-01-19 12:06:47 +0100

    Initial Commit

---
+++ find maximum/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  find maximum
+//
+//  Created by Michał GÓRNY on 19/01/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
