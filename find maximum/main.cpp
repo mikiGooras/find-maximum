ae06703f942263e044b08144198c98afb1110308
Michał Górny <michal.x.gorny@gmail.com>
2022-01-19 14:53:37 +0100

    app for indicating the highest and lowest value in a range

--- find maximum/main.cpp
+++ find maximum/main.cpp
@@ -7,8 +7,40 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+using namespace std;
+
+
+int counter;
+int *range;
+int maximum, minimum;
+
+ 
+
+int main() {
+    cout << "podaj ile liczb bedzie porównywanych ";
+    cin >> counter ;
+    range = new int [counter];
+    for (int i =0; i<counter; i++){
+        if (i==0){
+            cout<< "podaj pierwszą liczbę";
+            cin >> range[i];
+            maximum=range[i];
+            minimum=range[i];
+        }else{
+            cout << "podaj kolejną liczbę";
+            cin >> range[i];
+           if (maximum < range[i]){
+                maximum=range[i];
+            } else if (minimum> range[i]){
+                minimum=range[i];
+            }
+            
+            }
+        }
+    
+    cout << " najwyższa wartość wynosi" << maximum << endl;
+    cout << " najniższa wartość wynosi "<< minimum<< endl ;
+    
     return 0;
 }
